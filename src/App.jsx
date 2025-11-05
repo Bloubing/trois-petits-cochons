cd6b0b1b870af86bc4d476ad07cee090bc924cc8
Bloubing <140849997+Bloubing@users.noreply.github.com>
2025-11-05 20:35:13 +0100

    Ajout de styles à Maison

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,10 @@ import Maison from "./Maison";
 
 export default function App() {
   return (
-    <>
+    <div className="village">
       <Maison type="paille" nbPvDefaut={1} />
       <Maison type="bois" nbPvDefaut={2} />
       <Maison type="briques" nbPvDefaut={3} />
-    </>
+    </div>
   );
 }
--- src/Maison.jsx
+++ src/Maison.jsx
@@ -1,8 +1,8 @@
 export default function Maison({ type, nbPvDefaut }) {
   return (
-    <>
+    <div className={type}>
       <p>Maison en {type}</p>
       <p>({nbPvDefaut} PV)</p>
-    </>
+    </div>
   );
 }
