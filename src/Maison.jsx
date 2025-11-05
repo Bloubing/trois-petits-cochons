40ffa239ade9f922ca34b949a41fb47e56efc254
Bloubing <140849997+Bloubing@users.noreply.github.com>
2025-11-05 20:37:53 +0100

    Ajout d'un bouton Souffler à Maison

--- src/Maison.jsx
+++ src/Maison.jsx
@@ -1,8 +1,13 @@
 export default function Maison({ type, nbPvDefaut }) {
+  function handleSouffler() {
+    console.log("soufflé");
+  }
+
   return (
     <div className={type}>
       <p>Maison en {type}</p>
       <p>({nbPvDefaut} PV)</p>
+      <button onClick={handleSouffler}>Souffler</button>
     </div>
   );
 }
